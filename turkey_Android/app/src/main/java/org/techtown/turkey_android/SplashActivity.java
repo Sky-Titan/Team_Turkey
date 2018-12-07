f13ccef761abb00d77ec7dd87d54a140fe23b17b
Sky-Titan <er65119@naver.com>
2018-12-07 21:28:39 +0900

    image 폴더 추가

---
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/SplashActivity.java
@@ -0,0 +1,23 @@
+package org.techtown.turkey_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(3000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
