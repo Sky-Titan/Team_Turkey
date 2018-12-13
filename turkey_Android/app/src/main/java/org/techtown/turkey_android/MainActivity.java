b9c1e940b98e8dbbd29832f13371ff472bd1ac97
Sky-Titan <er65119@naver.com>
2018-12-13 23:40:52 +0900

    fragment전환구현

--- turkey_Android/app/src/main/java/org/techtown/turkey_android/MainActivity.java
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/MainActivity.java
@@ -1,32 +1,55 @@
 package org.techtown.turkey_android;
 
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+
 import android.support.design.widget.BottomNavigationView;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    MenuItem prevMenuItem;
+    FragmentA fragment_A;
+    FragmentB fragment_B;
+    FragmentC fragment_C;
+
+    private ViewPager mViewPager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fr = null;
+            FragmentManager fm =null;
+            FragmentTransaction fragmentTransaction=null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    mViewPager.setCurrentItem(0);
+                    return true ;
+
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mViewPager.setCurrentItem(1);
                     return true;
+
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mViewPager.setCurrentItem(2);
                     return true;
             }
+
             return false;
         }
     };
@@ -36,9 +59,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mViewPager=(ViewPager)findViewById(R.id.fragment_container);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if(prevMenuItem!=null){
+                    prevMenuItem.setChecked(false);
+                }
+                else {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                navigation.getMenu().getItem(i).setChecked(true);
+                prevMenuItem=navigation.getMenu().getItem(i);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        setupViewPager(mViewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        fragment_A = new FragmentA();
+        fragment_B = new FragmentB();
+        fragment_C = new FragmentC();
+        viewPagerAdapter.addFragment(fragment_A);
+        viewPagerAdapter.addFragment(fragment_B);
+        viewPagerAdapter.addFragment(fragment_C);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 
 }
