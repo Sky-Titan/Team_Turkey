c71a5ad6c97253a3472bb311c57f79bc5da5279c
Sky-Titan <er65119@naver.com>
2018-12-22 00:52:09 +0900

    강의교환게시판 검색

--- turkey_Android/app/src/main/java/org/techtown/turkey_android/FragmentC.java
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/FragmentC.java
@@ -2,12 +2,26 @@ package org.techtown.turkey_android;
 //fragment_c is for board
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -15,6 +29,18 @@ import android.widget.Button;
  */
 public class FragmentC extends Fragment {
 
+    String myJSON;
+    JSONArray posts = null;
+
+    EditText editText;
+
+    ListView listview;
+    PostViewAdapter adapter;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_CONTENT = "content";
+    private static final String TAG_PASSWORD = "password";
 
     public FragmentC() {
         // Required empty public constructor
@@ -26,6 +52,8 @@ public class FragmentC extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_c,container,false);
+
+        editText=(EditText)view.findViewById(R.id.post_edit);
         Button button=(Button)view.findViewById(R.id.f_c_btn_write);
         Button button2=(Button)view.findViewById(R.id.f_c_btn_search);
         button.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +63,134 @@ public class FragmentC extends Fragment {
                 startActivity(intent);
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().equals(""))//검색창에 아무 것도 없을시
+                {
+                    adapter = new PostViewAdapter() ;
+                    listview.setAdapter(adapter);
+                    getData("http://119.201.56.98/select_post.php");
+
+                }
+                else//검색어가 존재 할 시
+                {
+                    getData2("http://119.201.56.98/select_lecture.php");//SEARCH 용도의 getdata
+                    adapter = new PostViewAdapter() ;
+                    listview.setAdapter(adapter);
+                    try{
+                        JSONObject jsonObj = new JSONObject(myJSON);
+                        posts = jsonObj.getJSONArray(TAG_RESULTS);
+
+                        for(int i =0;i< posts.length();i++){
+                            JSONObject c = posts.getJSONObject(i);
+                            String id = c.getString(TAG_ID);
+                            String title = c.getString(TAG_TITLE);
+                            String content = c.getString(TAG_CONTENT);
+                            String password = c.getString(TAG_PASSWORD);
+
+                            if(title.contains(editText.getText().toString()))//해당 교과목번호에 검색어가 포함이 되어있으면 추가
+                            {
+                                adapter.addItem(id,title,content,password);
+                            }
+
+                        }
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+        adapter=new PostViewAdapter();
+        listview = (ListView) view.findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+        getData("http://119.201.56.98/select_post.php");
         return view;
     }
 
+    public void getData(String url){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params){
+                String uri = params[0];
+                BufferedReader bufferedReader = null;
+                try{
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine()) != null){
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+                }catch (Exception e){
+                    return null;
+                }
+            }
+            @Override
+            protected void onPostExecute(String result){
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+
+    public void getData2(String url){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params){
+                String uri = params[0];
+                BufferedReader bufferedReader = null;
+                try{
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine()) != null){
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+                }catch (Exception e){
+                    return null;
+                }
+            }
+            @Override
+            protected void onPostExecute(String result){
+                myJSON = result;
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
+    }
+    protected void showList(){
+        try{
+            JSONObject jsonObj = new JSONObject(myJSON);
+            posts = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i =0;i< posts.length();i++){
+                JSONObject c = posts.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String title = c.getString(TAG_TITLE);
+                String content = c.getString(TAG_CONTENT);
+                String password = c.getString(TAG_PASSWORD);
+
+                adapter.addItem(id,title,content,password);
+
+            }
+            listview.setAdapter(adapter);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
 }
---
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/PostViewItem.java
@@ -0,0 +1,41 @@
+package org.techtown.turkey_android;
+
+public class PostViewItem {
+    String id;
+    String title;
+    String content;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
