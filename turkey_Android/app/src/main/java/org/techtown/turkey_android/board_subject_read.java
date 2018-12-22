17532f8bb8c9072713260088d4a9a9e9d944487b
unknown <tpgus123123@naver.com>
2018-12-22 18:02:09 +0900

    강의별 게시판 읽기 삭제 수정

--- turkey_Android/app/src/main/java/org/techtown/turkey_android/board_subject_read.java
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/board_subject_read.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -47,7 +48,7 @@ public class board_subject_read extends AppCompatActivity {
 
         Intent intent_sr = getIntent();
         check_id = intent_sr.getExtras().getString("id");// intent로 board_subject로부터 id 값을 가져옴
-
+        Log.i("board_subject_read",check_id);
         getData2("http://119.201.56.98/select_checked_post.php?id0="+check_id);////php수정필요한부분
 
     }
@@ -134,10 +135,9 @@ public class board_subject_read extends AppCompatActivity {
 
     public void bsr_del(View view)
     {
-        EditText scr_password = (EditText)findViewById(R.id.scr_password);
+        EditText bsr_password = (EditText)findViewById(R.id.bsr_password);
 
-
-        if(scr_password.getText().toString().equals(password2check)) {
+        if(bsr_password.getText().toString().equals(password2check)) {
             board_subject_read.DeleteData task = new board_subject_read.DeleteData();
             task.execute("http://119.201.56.98/delete_post.php",check_id);//php수정필요한부분
             Toast.makeText(getApplicationContext(), "삭제되었습니다.", Toast.LENGTH_SHORT).show();
--- turkey_Android/app/src/main/java/org/techtown/turkey_android/board_subject_write.java
+++ turkey_Android/app/src/main/java/org/techtown/turkey_android/board_subject_write.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,21 +21,22 @@ public class board_subject_write extends AppCompatActivity {
     EditText bsw_title;
     EditText bsw_content;
     EditText bsw_password;
-    String number;
+    String subjectNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_subject_write);
-
         bsw_title = (EditText)findViewById(R.id.bsw_title);
         bsw_content = (EditText)findViewById(R.id.bsw_content);
         bsw_password = (EditText)findViewById(R.id.bsw_password);
+
         // 과목코드는 intent로 받아온다.
         Intent intent_sw = getIntent();
-        number = intent_sw.getExtras().getString("subjectNumber");// intent로 board_subject로부터 subjectNumber 값을 가져옴
+        subjectNumber = intent_sw.getExtras().getString("subjectNumber");// intent로 board_subject로부터 subjectNumber 값을 가져옴
+        Log.i("b_s_w",subjectNumber);
     }
 
-    public void scw_add(View view)
+    public void bsw_add(View view)
     {
         String text4err="";
         if(bsw_title.length()>0&&bsw_content.length()>0&&bsw_password.length()>0) {
@@ -43,7 +45,7 @@ public class board_subject_write extends AppCompatActivity {
             String content = bsw_content.getText().toString();
             String password = bsw_password.getText().toString();
             board_subject_write.InsertData task = new board_subject_write.InsertData();
-            task.execute("http://119.201.56.98/insert_post.php",number,title,content,password);//php수정필요함
+            task.execute("http://119.201.56.98/insert_post.php",subjectNumber,title,content,password);//php수정필요함
             Toast.makeText(getApplicationContext(), "추가했습니다.", Toast.LENGTH_SHORT).show();
 
             finish();
